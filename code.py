09223dec9302bbcc68e4ac9bd59eff8fdddf33f5
Joxzes <josedavidlopez2013@gmail.com>
2024-11-18 08:17:09 -0500

    Cambio en el codigo

--- code.py
+++ code.py
@@ -11,30 +11,36 @@ from Bio.Data import CodonTable
 p53 = SeqIO.read('p53.fa', "fasta")
 pato = SeqIO.read('pato_1.fa', "fasta").seq
 seq = p53.seq
-output_file = "lpato_5.fa"
 posicion = 569
 mutation = "T"
 location = 142 + (posicion - 1)
 nucleotido_actual = p53.seq[location]
 
-# print(seq[location])
-# print(pato[location])
-
 print(f"Nucleótido actual en la posición {location}: {nucleotido_actual}")
 
-# Realizar el cambio
 secuencia_modificada = (
     p53.seq[:location] + mutation + p53.seq[location + 1:]
 )
 
-# Crear un nuevo registro FASTA con la secuencia modificada
+proteina = secuencia_modificada[142:]
+translated_seq = proteina.translate(to_stop=True)
+
 p53_modificado = SeqRecord(
     Seq(secuencia_modificada),
     id=p53.id + "_modificado",
     description=f"Secuencia con cambio en posición {location} ({nucleotido_actual}->{mutation})"
 )
 
-# Guardar la secuencia modificada en un archivo
+translated_record = SeqRecord(
+    translated_seq,
+    id=p53.id,
+    description="Proteína traducida hasta el primer codón de parada"
+)
+
+output_file = "lpato_5.fa"
+output_file = "p53_prot.fa"
+SeqIO.write(translated_record, output_file, "fasta")
 SeqIO.write(p53_modificado, output_file, "fasta")
 
 print(f"Secuencia modificada guardada en '{output_file}'.")
+print(f"Secuencia traducida guardada en: {output_file}")
